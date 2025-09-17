e65ab464633cb0fbb517e9ffc640e65b0121ded6
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-17 17:02:25 +0530

    Add theme button for theme switching

---
+++ src/components/theme_button.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeBtn() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      className="fixed z-10 bottom-6 text-4xl right-4 cursor-pointer"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme==="light"?"🌙":"🔆"}
+    </button>
+  );
+}
