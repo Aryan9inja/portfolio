7947d257745ef839eb7b4621c91d2a86a735532e
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-17 17:04:01 +0530

    Add header image and remove excess code

--- src/components/Home/header.tsx
+++ src/components/Home/header.tsx
@@ -1,22 +1,24 @@
-import { TypewriterEffect } from "../ui/typewriter-effect";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import { words } from "./words";
 
 export default function Header() {
-  const words = [
-    { text: "A" },
-    { text: "passionate" },
-    { text: "full" },
-    { text: "-" },
-    { text: "stack" },
-    { text: "developer" },
-  ];
   return (
-    <header className="text-text font-playfair">
-      <p className="text-2xl md:text-4xl mb-1 md:mb-2 font-bold">Hi! 🤟</p>
-      <p className="text-4xl md:text-6xl mb-2 md:mb-4">
-        I&apos;m <span className="text-accent font-semibold font-roboto">Aryan</span> Singh
-        Thakur
-      </p>
-      <TypewriterEffect words={words} className="font-bitcount font-medium tracking-tighter text-left text-sm"/>
+    <header className="text-text font-playfair flex items-center md:justify-around">
+      <div className="w-full md:w-[55%]">
+        <p className="text-2xl md:text-4xl mb-1 md:mb-2 font-bold">Hi! 🤟</p>
+        <p className="text-4xl md:text-6xl mb-1 md:mb-2">
+          I&apos;m{" "}
+          <span className="text-accent font-semibold font-roboto">Aryan</span>{" "}
+          Singh Thakur
+        </p>
+        <TypewriterEffect
+          words={words}
+          className="font-bitcount font-semibold text-left md:text-left"
+        />
+      </div>
+      <div className="hidden md:block">
+        <img src="/dev.png" />
+      </div>
     </header>
   );
 }
