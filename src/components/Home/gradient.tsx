eca4fd32261fbc9cb50c43fe838b03496802230d
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-18 23:30:09 +0530

    Add gradient horizon component

---
+++ src/components/Home/gradient.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Gradient() {
+  return (
+    <section className="relative h-[50px] md:h-[150px] overflow-hidden mt-30 mb-10">
+      <div className="absolute left-[-250%] h-[6000%] w-[600%] rounded-full bg-gradient-to-b from-accent from-0% to-bg to-[0.5%]"></div>
+    </section>
+  );
+}
