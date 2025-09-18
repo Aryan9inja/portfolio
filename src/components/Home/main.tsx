b4343218e785b2a0ecc9c8ffddf0db88929ee0b7
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-18 23:31:28 +0530

    Create main section to show skills

---
+++ src/components/Home/main.tsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <div>
+      <h1 className="text-6xl text-text text-shadow-current text-shadow-lg text-center">
+        Skills
+      </h1>
+    </div>
+  );
+}
