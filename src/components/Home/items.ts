7ebba038916de0fad31a5b6a52b4cb2f4a3e2749
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-21 18:50:33 +0530

    Add skill items and technologies

---
+++ src/components/Home/items.ts
@@ -0,0 +1,96 @@
+import { Item } from "./types";
+import {
+  siAppwrite,
+  siBootstrap,
+  siC,
+  siCoffeescript,
+  siCplusplus,
+  siCss,
+  siExpress,
+  siGit,
+  siGithub,
+  siGo,
+  siHtml5,
+  siJavascript,
+  siJest,
+  siJupyter,
+  siMongodb,
+  siMongoose,
+  siNestjs,
+  siNetlify,
+  siNextdotjs,
+  siNodedotjs,
+  siNumpy,
+  siPandas,
+  siPostgresql,
+  siPostman,
+  siPython,
+  siReact,
+  siReacthookform,
+  siRedux,
+  siRender,
+  siScikitlearn,
+  siShadcnui,
+  siSupabase,
+  siTailwindcss,
+  siTypescript,
+  siVercel,
+  siVitest,
+  siZod,
+} from "simple-icons";
+
+export const backendItems: Item[] = [
+  { name: "Mongo DB", logo: siMongodb },
+  { name: "Next JS", logo: siNextdotjs },
+  { name: "Express JS", logo: siExpress },
+  { name: "Node JS", logo: siNodedotjs },
+  { name: "Postgre SQL", logo: siPostgresql },
+  { name: "Supabase", logo: siSupabase },
+  { name: "Appwrite", logo: siAppwrite },
+  { name: "Nest JS", logo: siNestjs },
+  { name: "Mongoose", logo: siMongoose },
+];
+
+export const frontendItems: Item[] = [
+  { name: "HTML 5", logo: siHtml5 },
+  { name: "CSS 3", logo: siCss },
+  { name: "React JS", logo: siReact },
+  { name: "Tailwind CSS", logo: siTailwindcss },
+  { name: "Shadcn ui", logo: siShadcnui },
+  { name: "Bootstrap", logo: siBootstrap },
+  { name: "Next JS", logo: siNextdotjs },
+  { name: "Redux-Toolkit", logo: siRedux },
+  { name: "React-hook-form", logo: siReacthookform },
+];
+
+export const otherToolsAndTechItems: Item[] = [
+  { name: "Postman", logo: siPostman },
+  { name: "Typescript", logo: siTypescript },
+  { name: "Git", logo: siGit },
+  { name: "Github", logo: siGithub },
+  { name: "Jest", logo: siJest },
+  { name: "Vitest", logo: siVitest },
+  { name: "Zod", logo: siZod },
+];
+
+export const devOps: Item[] = [
+  { name: "Vercel", logo: siVercel },
+  { name: "Netlify", logo: siNetlify },
+  { name: "Render", logo: siRender },
+];
+
+export const AI_ML: Item[] = [
+  { name: "Numpy", logo: siNumpy },
+  { name: "Pandas", logo: siPandas },
+  { name: "Jupyter", logo: siJupyter },
+  { name: "Scikit Learn", logo: siScikitlearn },
+];
+
+export const languages:Item[]=[
+  {name:"Javascript",logo:siJavascript},
+  {name:"Python",logo:siPython},
+  {name:"C",logo:siC},
+  {name:"C++",logo:siCplusplus},
+  {name:"Java",logo:siCoffeescript},
+  {name:"Go",logo:siGo}
+]
