06107c92a5a3f111d4f223790ad3d95616c04b61
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-28 15:42:08 +0530

    Add project data for portfolio projects

---
+++ src/components/Projects/projectData.tsx
@@ -0,0 +1,28 @@
+import { ProjectProps } from "./projectTemplate";
+
+export const Projects: ProjectProps[] = [
+  {
+    imageUrldark: "/projects/portly.png",
+    imageUrlLight: "/projects/portly_light.png",
+    name: "Portly",
+    intro: "AI Powered Resume to Portfolio Website Builder",
+  },
+  {
+    imageUrldark: "/projects/flowceipt.png",
+    imageUrlLight: "/projects/flowceipt_light.png",
+    name: "Flowceipt",
+    intro: "AI-Powered Receipt & Expense Manager",
+  },
+  {
+    imageUrldark: "/projects/synote.png",
+    imageUrlLight: "/projects/synote_light.png",
+    name: "Synote",
+    intro: "Smart Note/Tasks management app",
+  },
+  {
+    imageUrldark: "/projects/shutr.png",
+    imageUrlLight: "/projects/shutr_light.png",
+    name: "Shutr",
+    intro: "Instagram-inspired Social Media Platform",
+  },
+];
