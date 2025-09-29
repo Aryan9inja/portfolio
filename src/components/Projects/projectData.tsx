92c1a8840414a8c8cfa32da41e30c92caa8a90af
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-09-29 18:42:58 +0530

    Add slugs for project entries in projectData

--- src/components/Projects/projectData.tsx
+++ src/components/Projects/projectData.tsx
@@ -6,23 +6,27 @@ export const Projects: ProjectProps[] = [
     imageUrlLight: "/projects/portly_light.png",
     name: "Portly",
     intro: "AI Powered Resume to Portfolio Website Builder",
+    slug: "/portly",
   },
   {
     imageUrldark: "/projects/flowceipt.png",
     imageUrlLight: "/projects/flowceipt_light.png",
     name: "Flowceipt",
     intro: "AI-Powered Receipt & Expense Manager",
+    slug: "/flowceipt",
   },
   {
     imageUrldark: "/projects/synote.png",
     imageUrlLight: "/projects/synote_light.png",
     name: "Synote",
     intro: "Smart Note/Tasks management app",
+    slug: "/synote",
   },
   {
     imageUrldark: "/projects/shutr.png",
     imageUrlLight: "/projects/shutr_light.png",
     name: "Shutr",
     intro: "Instagram-inspired Social Media Platform",
+    slug: "/shutr",
   },
 ];
